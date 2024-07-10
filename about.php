a35f02fbfe97bde2ea0045a6ca27c2b0276d4b5c
jakir0hossen <jhbjakir@gmail.com>
2024-07-11 01:12:38 +0600

    first commit

---
+++ about.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>LU Magazine</title>
+    <script src="https://kit.fontawesome.com/eaa4609b2f.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/79d1bfb033.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./about.css" />
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <div class="logo">
+                <img src="images/logo1.png" alt="">
+            </div>
+            
+
+            <div class="navbar">
+                <ul>
+                    <li><a href="Home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="#">LU</a>
+                     <ul>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Book</a></li>
+                        <li><a href="#">Entertainment</a></li>
+                        <li><a href="#">Event</a></li>
+                        <li><a href="#">Lu Club</a>
+                            <ul>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUSSC</a></li>
+                                <li><a href="#">LUEC</a></li>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUDC</a></li>
+                                <li><a href="#">LUMUNA</a></li>
+                                <li><a href="#">LU BNCC</a></li>
+                                <li><a href="#">LUPS</a></li>
+                                <li><a href="#">LUPS</a></li>
+                            </ul>
+                        </li>
+                     </ul>
+                    </li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Gallery</a>
+                        
+                        <ul>
+                            <li><a href="#">Photo</a></li>
+                            <li><a href="#">Video</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="registration.php">Sign Up</a></li>
+                    <li><a href="login.php"> <i class="fa-solid fa-right-to-bracket"></i> Login</a></li>
+                </ul>
+                
+            </div>
+
+        </div>
+    </nav>
+
+    <header class="showcase">
+        <div class="container">
+            <div class="text-content">
+                <p class="lu-category">LU Magazine</p>
+                <h1>some LU Magazine article</h1>
+                <p>
+                    The university's annual science fair, a showcase of student research and innovation, captivated attendees with projects ranging from sustainable energy solutions to cutting-edge medical advancements. The event highlighted the university's commitment to fostering creativity and academic excellence.
+                </p>
+                <a href="./html/article.html">Read More</a>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <h2>About Lu Club</h2>
+        <p>Lu Club is a place where bright minds meet to exchange ideas, explore new concepts, and collaborate on innovative projects.</p>
+    </section>
+    <section>
+        <h2>Upcoming Events</h2>
+        <ul>
+            <li>Workshop on Creative Writing - May 20, 2024</li>
+            <li>Guest Lecture on Artificial Intelligence - June 5, 2024</li>
+            <li>Networking Mixer - June 15, 2024</li>
+        </ul>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="footer">
+                <article>
+                    <div class="logo">
+                        <img src="images/logo.png" alt="">
+                    </div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
+                        deserunt assumenda enim non? Ratione ipsum voluptates fuga eos
+                        earum vitae.
+                    </p>
+                </article>
+
+                <article>
+                    <h4>EMAIL NEWSLETTER</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <input type="email" placeholder="Enter Email..." />
+                    <a href="#"> Subscribe</a>
+                </article>
+
+                <article>
+                    <h4>SITE LINKS</h4>
+                    <p>Help & Support</p>
+                    <p>Privacy Policy</p>
+                    <p>About Us</p>
+                    <p>Contact</p>
+                </article>
+
+                <article>
+                    <h4>JOIN OUR CLUB</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Obcaecati, maiores!
+                    </p>
+                    <a href="#">Join Now</a>
+                </article>
+            </div>
+
+            <div class="copyright">
+                <p>Copyright &copy; 2024, All Rights Reserved</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./js/home.js"></script>
+</body>
+
+</html>
